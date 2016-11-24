4ac3d8d0af7552bb87f299c7e168644bffd17dc5
Toby Chow <toby_chow@hotmail.com>
2016-11-24 15:47:19 -0500

    before animations

--- personalprofile.js
+++ personalprofile.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
-                    scrollTop: target.offset().top
+                    scrollTop: target.offset().top - 39
                 }, 1000);
                 return false;
             }
